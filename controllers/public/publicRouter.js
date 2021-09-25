f5efc7096eb660f708df159a9bdd9f4101fc8c53
heidi <hayriye_oeztuerk@web.de>
2021-09-25 17:47:16 +0200

    commit

---
+++ controllers/public/publicRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const publicRouter = express.Router();
+
+publicRouter
+.get('/', (req, res) => res.render('pages/notimplemented'))
+  .get('/login', (req, res) => res.render('pages/notimplemented'))
+  .get('/news', (req, res) => res.render('pages/notimplemented'))
+  .get('/news/:id', (req, res) => res.render('pages/notimplemented'))
+  .get('/hikers', (req, res) => res.render('pages/notimplemented'))
+  .get('/hikers/:id', (req, res) => res.render('pages/notimplemented'))
+  .get('/hikes', (req, res) => res.render('pages/notimplemented'))
+  .get('/hikes/:id', (req, res) => res.render('pages/notimplemented'))
+
+  .post('/userapi/hikers', (req, res) => res.status(400).send("not implemented yet"))
+  .put('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
+  .delete('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
+  .put('/userapi/hikes/:id', (req, res) => res.status(400).send("not implemented yet"))
+
+
+
+module.exports = publicRouter
--- index.js
+++ index.js
@@ -4,31 +4,21 @@ const PORT = process.env.PORT || 5000
 
 const newsRouter = require('./controllers/api/newsRouter');
 const hikersRouter = require('./controllers/api/hikersRouter');
+const publicRouter = require('./controllers/public/publicRouter');
+
 
 express()
   .use(express.json())
   .use(express.static(path.join(__dirname, 'public')))
   .use('/api/news', newsRouter)
   .use('/api/hikers', hikersRouter)
+  .use('/', publicRouter)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/notimplemented'))
-  .get('/login', (req, res) => res.render('pages/notimplemented'))
-  .get('/news', (req, res) => res.render('pages/notimplemented'))
-  .get('/news/:id', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikers', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikers/:id', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikes', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikes/:id', (req, res) => res.render('pages/notimplemented'))
-
-  .post('/userapi/hikers', (req, res) => res.status(400).send("not implemented yet"))
-  .put('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
-  .delete('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
-  .put('/userapi/hikes/:id', (req, res) => res.status(400).send("not implemented yet"))
-
+  
 
   .get('/api/hikes', (req, res) => {
     res.send(getHikes())
   })
-  
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
