eef30af43244fab6689716fb00e71fbc2596703e
Erik <elaktrato@gmail.com>
2021-10-13 20:38:36 +0200

    Setup EJS and Template

--- controllers/public/publicRouter.js
+++ controllers/public/publicRouter.js
@@ -2,19 +2,19 @@ const express = require('express');
 const publicRouter = express.Router();
 
 publicRouter
-.get('/', (req, res) => res.render('pages/notimplemented'))
-  .get('/login', (req, res) => res.render('pages/notimplemented'))
-  .get('/news', (req, res) => res.render('pages/notimplemented'))
-  .get('/news/:id', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikers', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikers/:id', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikes', (req, res) => res.render('pages/notimplemented'))
-  .get('/hikes/:id', (req, res) => res.render('pages/notimplemented'))
+    .get('/', (req, res) => res.render('pages/notimplemented'))
+    .get('/login', (req, res) => res.render('pages/notimplemented'))
+    .get('/news', (req, res) => res.render('pages/notimplemented'))
+    .get('/news/:id', (req, res) => res.render('pages/notimplemented'))
+    .get('/hikers', (req, res) => res.render('pages/notimplemented'))
+    .get('/hikers/:id', (req, res) => res.render('pages/notimplemented'))
+    .get('/hikes', (req, res) => res.render('pages/notimplemented'))
+    .get('/hikes/:id', (req, res) => res.render('pages/hikePage'))
 
-  .post('/userapi/hikers', (req, res) => res.status(400).send("not implemented yet"))
-  .put('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
-  .delete('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
-  .put('/userapi/hikes/:id', (req, res) => res.status(400).send("not implemented yet"))
+.post('/userapi/hikers', (req, res) => res.status(400).send("not implemented yet"))
+    .put('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
+    .delete('/userapi/hikers/:id', (req, res) => res.status(400).send("not implemented yet"))
+    .put('/userapi/hikes/:id', (req, res) => res.status(400).send("not implemented yet"))
 
 
 
