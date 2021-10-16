a962048e2c90f4eda5264b041a0f8967b89d81d5
Erik <elaktrato@gmail.com>
2021-10-16 14:02:52 +0200

    Make Hike overview page

--- controllers/public/publicRouter.js
+++ controllers/public/publicRouter.js
@@ -2,11 +2,10 @@ const express = require('express');
 const publicRouter = express.Router();
 const { getHikes, getHikesById } = require('../../models/dataService')
 
+
 publicRouter
     .get('/', async(req, res) => {
-        //Todo fetch hikes data with data sevice and pass it to template
-        const hikes = await getHikes()
-        res.render('pages/hikes', { hikes })
+        res.render('pages/hikes', { hikes: await getHikes() })
 
     })
     .get('/login', (req, res) => res.render('pages/notimplemented'))
@@ -14,11 +13,11 @@ publicRouter
     .get('/news/:id', (req, res) => res.render('pages/notimplemented'))
     .get('/hikers', (req, res) => res.render('pages/notimplemented'))
     .get('/hikers/:id', (req, res) => res.render('pages/notimplemented'))
-    .get('/hikes', (req, res) => res.render('pages/notimplemented'))
+    .get('/hikes', async(req, res) => {
+        res.render('pages/hikeslist', { hikes: await getHikes() })
+    })
     .get('/hikes/:id', async(req, res) => {
-        const hike = await getHikesById(req.params.id);
-        console.log(hike)
-        res.render('pages/hikePage', { hike })
+        res.render('pages/hikePage', { hike: await getHikesById(req.params.id) })
     })
 
 .post('/userapi/hikers', (req, res) => res.status(400).send("not implemented yet"))
